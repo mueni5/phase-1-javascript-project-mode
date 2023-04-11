7afdfaadc55516e465197596594a123906257a4e
+ <+>
2023-04-11 14:17:04 +0300

    final

--- Js/index.js
+++ Js/index.js
@@ -23,6 +23,25 @@ fetch('db.json', {
 .then(response => response.json())
 .then(data => console.log(data.tasks));
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "'title'": "Go to the mosque",
+  "completed'": true
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:3000/tasks", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 
 const form = document.querySelector('form');
 const input = document.querySelector('#new-item');
--- README.md
+++ README.md
@@ -8,17 +8,6 @@
 - Debug issues in small- to medium-sized projects
 - Build and iterate on a project MVP
 
-## Introduction
-
-Welcome to JavaScript Project Mode!
-
-You’ve worked so hard to get here and have learned a ton. Now it's time to bring
-it all together!
-
-For this project, you're going build a Single Page Application (**SPA**).
-Building this application will be challenging because it will integrate
-everything you've learned up to this point. Your frontend will be built with
-HTML, CSS, and JavaScript and will communicate with a public API.
 
 ### Project Requirements
 
